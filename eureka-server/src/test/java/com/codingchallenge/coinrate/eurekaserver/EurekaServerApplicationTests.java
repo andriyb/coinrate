b161e69beb07860879e0add82c9c3cb7cfd4a821
Andriy Bakhmet <bakhmet@gmail.com>
2023-06-17 23:21:33 +0200

    Created Eureka Server Web Application.

---
+++ eureka-server/src/test/java/com/codingchallenge/coinrate/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingchallenge.coinrate.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
